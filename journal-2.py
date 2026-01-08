5eb83bdd4cf616b16be7cf3d8cebe85d83ac91bc
CarpDiemm <117697566+CarpDiemm@users.noreply.github.com>
2026-01-07 20:50:10 -0800

    Add files via upload

---
+++ journal-2.py
@@ -0,0 +1,32 @@
+# Sum of two floating point values
+x = 25.72
+print("type(x): ",type(x))
+
+y = 102.84
+print("type(y): ",type(y))
+
+
+print("x+y =",x+y)
+print("type(x+y): ",type(x+y))
+
+
+# Difference between two integer values
+x = 72
+print("type(x): ",type(x))
+
+y = 17
+print("type(y): ",type(y))
+
+print("x-y =",x-y)
+print("type(x-y): ",type(x-y))
+
+
+# Product of floating point and integer values
+x = 116.25
+print("type(x): ",type(x))
+
+y = 72
+print("type(y): ",type(y))
+
+print("x*y =",x*y)
+print("type(x*y): ",type(x*y))
